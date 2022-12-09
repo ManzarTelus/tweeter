9cecbde9832cb072324fe5308b8edb21bb730969
Manzar <manzar.hussain@telus.com>
2022-12-08 23:38:04 -0500

    Form Submission using JQuery & Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,71 +3,70 @@
  * jQuery is already loaded
   */
 
-$(document).ready(() {
+$(document).ready(function() {
 
-  //Hardcoded Tweets Data
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-const createTweetElement = function (tweet) {
-  let $tweet = `
-  <article class="tweet">
-    <header class="tweet-header">
-      <div class="name-avatar">
-        <div>
-          <img src="${data.user.avatars}"></img>
-          <p>${data.user.name}</p>
+  const createTweetElement = function (tweet) {
+    const $tweet = $(`
+    <article class="tweet">
+      <header class="tweet-header">
+        <div class="name-avatar">
+          <div>
+            <img src="${tweet.user.avatars}"></img>
+            <p>${tweet.user.name}</p>
+          </div>
+          <p class="last-name">${tweet.user.handle}</p>
         </div>
-        <p class="last-name">${data.user.handle}</p>
+      </header>
+      <p class="content">${tweet.content.text}</p>
+      <footer class="tweet-footer">
+        <div>
+          <p>${timeago.format(tweet.created_at)}</p>
+        </div>
+        <div class="image-class">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
       </div>
-    </header>
-    <p class="content">${data.content.text}</p>
-    <footer class="tweet-footer">
-      <div>
-        <p>${data.created_at}</p>
-      </div>
-      <div class="image-class">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </div>
-    </footer>
-  </article>`
-return $tweet;
-}
-
-const renderTweets = function (tweets) {
-
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets containe
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    const $alltweets = $('.tweet-container');
-    $alltweets.append($tweet);
+      </footer>
+    </article>`);
+  return $tweet;
+  };
+  
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and prepend it to the tweets container
+    $('.tweet-container').empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.tweet-container').prepend($tweet);
+    }
+  };
+  
+  const fetchTweetsWithAjax = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'get',
+    }).then(renderTweets)
+  };
+  
+  $('#form').submit(function(event){
+    const tweetData =  $(this).serialize();
+  if (tweetData.length === 0) {
+    event.preventDefault();
+    alert ("Please write TWEET before submission.");
+  } else if(tweetData.length > 140) {
+    event.preventDefault();
+    alert ("The text entered exceeds the maximum length!");
+  } else {
+    event.preventDefault(); //prevent default action 
+  $("#form")[0].reset();
+    $.ajax({
+      url : '/tweets',
+      method: 'post',
+      tweetData
+    }).then(fetchTweetsWithAjax);
   }
-};
-renderTweets(data);
-})
+  })
+  })
+  
