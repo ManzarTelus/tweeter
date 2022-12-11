6578a6734fc0c136d85ed7ce9115d96840e722e0
Manzar <manzar.hussain@telus.com>
2022-12-10 23:34:14 -0500

    Bugs fixed -- running as per project requirments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,40 +11,40 @@ $(document).ready(function() {
     <article class="tweet">
       <header class="tweet-header">
         <div class="name-avatar">         
-            <img name=avatarPic src=${tweet.user.avatars}>
-            <div class="client-name" name="name">${tweet.user.name}</div>
+            <img name=avatar src=${tweet.user.avatars}>
+            <div class="client" name="name">${tweet.user.name}</div>
           </div>
           <div class="handle" name="handle">${tweet.user.handle}</div>
       </header>
       <div class="tweet-text" name="tweetText">${tweet.content.text}</div> 
       <footer>
-          <div>${timeago.format(tweet.created_at)}</div>        
+        <div>${timeago.format(tweet.created_at)}</div>        
         <div class="image-class">
         <i class="fa-solid fa-flag"></i>
         <i class="fas fa-retweet"></i>
         <i class="fa-solid fa-heart"></i>
-      </div>
+        </div>
       </footer>
-    </article>)
+    </article>
     `);
     return $tweet;
   };
   
   //loops through the tweets and dynamically render each
   const renderTweets = function(tweets) {
-    $('.tweets-container').empty();
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $('.tweets-container').prepend($tweet);
+    $('.tweet-container').empty();
+    for (const key of tweets) {
+      const $tweet = createTweetElement(key);
+      $('.tweet-container').prepend($tweet);
     }
   };
 
   $('#form').submit(function(event) {
     event.preventDefault();
     $('#error-print').slideUp(400).empty();
-    const text = $("#tweet-text").val();
+    const tweet = $("#tweet-text").val();
     const data = $(this).serialize();
-    if (!text) {
+    if (!tweet) {
       $('#error-print').empty();
       const $errorMessage ="⚠  Please write TWEET before submission. ⚠ ";
       $('#error-print').text($errorMessage).slideDown();
@@ -52,7 +52,7 @@ $(document).ready(function() {
       $('#error-print').css('color', 'red' );
       return;
     }
-    else if (text.length > 140) {
+    else if (tweet.length > 140) {
       const $errorMessage = "⚠ Too long. Plz rspct our arbitrary limit of 140 chars. #kthxbye. ⚠";
       $('#error-print').text($errorMessage).slideDown();
       $('#error-print').css('border-style', 'solid');
@@ -71,8 +71,8 @@ $(document).ready(function() {
 //Get tweets from the database and called the render function
 const loadTweets = function() {
   $.ajax({
-    url: '/tweets',
-    method: 'get',
+        method: 'get',
+        url: '/tweets',
   }).then(renderTweets);
 };
 $('.right-nav').click(function(event) {
